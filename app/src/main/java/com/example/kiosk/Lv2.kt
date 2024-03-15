91f3d038a833c3ca44f8c57d154984548e50b2c9
AndroidJeong <okbamtol@gmail.com>
2024-03-15 10:57:01 +0900

    level2,3 수정

--- app/src/main/java/com/example/kiosk/Lv2.kt
+++ app/src/main/java/com/example/kiosk/Lv2.kt
@@ -11,35 +11,21 @@ open class Food {
     open fun detailMenu(num: Int) {
         println("${name[num]}메뉴의 자세한 설명입니다.")
     }
+
 }
 
 class Burger : Food() {
-
-    override var name: ArrayList<String>
-        get() = super.name
-        set(value) {}
-
-    override var price: ArrayList<Int>
-        get() = super.price
-        set(value) {}
 
     val burgerMenu = arrayListOf<String>("버거1", "버거2", "버거3")
     val burgerPrice = arrayListOf<Int>(1, 2, 3)
 
     init {
-        for (i in burgerMenu) {
-            name.add(i)
-        }
-        // name = burgerMenu
-        // name = arrayListOf<String>("버거1","버거2","버거3")
-        for (i in burgerPrice) {
-            price.add(i)
-        }
-        // price.add(listOf(burgerPrice))
+        name = burgerMenu
+        price = burgerPrice
     }
 
     override fun detailMenu(num: Int) {
-        super.detailMenu(num)
+        println("${name[num]}의 가격은 ${price[num]}입니다")
     }
 }
 
@@ -47,7 +33,6 @@ class Burger : Food() {
 fun main() {
     val list = Burger()
     for (i in 0..list.name.count() - 1) {
-        println("${list.name[i]}의 가격은 ${list.price[i]}입니다")
         list.detailMenu(i)
     }
 
